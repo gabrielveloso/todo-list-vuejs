3c33a675d10fecaacc8f8637fc835d894fe31c79
Gabriel Veloso <gabrielveloso2@gmail.com>
2025-10-30 00:01:19 +0100

    todo list with vue

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script lang="ts" setup>
+import { computed, ref } from "vue";
+import { Task } from "./types/task";
+import ListTasks from "./components/ListTasks.vue";
+
+const tasks = ref<Task[]>([]);
+const newTaskTitle = ref("");
+const filter = ref("all");
+
+let nextTaskId = 1;
+
+const addTask = () => {
+  if (newTaskTitle.value.trim() !== "") {
+    console.log("Adding task:", newTaskTitle.value);
+
+    tasks.value.push(new Task(nextTaskId++, newTaskTitle.value, false));
+  }
+};
+
+const tasksFiltered = computed(() => {
+  if (filter.value === "all") {
+    return tasks.value;
+  } else if (filter.value === "completed") {
+    return tasks.value.filter((task) => task.completed);
+  } else if (filter.value === "pending") {
+    return tasks.value.filter((task) => !task.completed);
+  }
+  return tasks.value;
+});
+</script>
+
+<template>
+  <div class="app">
+    <h1>Task List</h1>
+    <input
+      v-model="newTaskTitle"
+      placeholder="Enter new task"
+      @keyup.enter="addTask"
+    />
+    <div class="btn-group">
+      <button class="button outline" @click="addTask">Add Task</button>
+    </div>
+
+    <br />
+    <hr />
+    <div class="filter">
+      <button class="button outline" @click="filter = 'all'">All</button>
+      <button class="button outline" @click="filter = 'completed'">
+        Completed
+      </button>
+      <button class="button outline" @click="filter = 'pending'">
+        Pending
+      </button>
+    </div>
+    <ListTasks :tasksFiltered="tasksFiltered" />
+  </div>
+</template>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.filter {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 20px;
+}
+
+.btn-group {
+  display: flex;
+  justify-content: flex-end;
+}
+
+button {
+  justify-self: end;
+  padding: 10px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/ListTasks.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { Task } from "../types/task";
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  tasksFiltered: Task[];
+}>();
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li class="item" v-for="task in tasksFiltered" :key="task.id">
+        {{ task.name }} - {{ task.completed ? "Done" : "Pending" }}
+        <button
+          class="button-complete"
+          v-if="!task.completed"
+          @click="task.completed = true"
+        >
+          Mark as done
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.button-complete {
+  background-color: gray;
+}
+
+.item {
+  display: flex;
+  width: 100%;
+  padding: 10px;
+  min-height: 70px;
+  justify-content: space-between;
+  align-items: center;
+  border: 3px solid #ccc;
+}
+</style>
